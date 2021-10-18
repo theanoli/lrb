c583c9e10ed5bbddc24e55db2bbd3ca92130c328
theano <theano@princeton.edu>
2021-10-18 15:32:42 -0400

    fancify runscript

--- runscript.sh
+++ runscript.sh
@@ -1,23 +1,34 @@
 # docker run -it -v ${YOUR TRACE DIRECTORY}:/trace sunnyszy/webcachesim ${traceFile} ${cacheType} ${cacheSize} [--param=value]
 root="/home/theano/webcachesim"
 export PATH="$root/build/bin"
-export WEBCACHESIM_TRACE_DIR="/local/disk1/traces"
+export WEBCACHESIM_TRACE_DIR="/home/theano/traces"
 export WEBCACHESIM_ROOT=$root
 
-# cacheSize=$((1024 * 1024 * 1024 * 142))
-# nearlystop=2000000000
-# traceFile=2020-01-19-20-21-22_samesize.tr
+trace="ntg1"
 
-cacheSize=$((1024 * 1024 * 1024 * 78))
-nearly_stop=1000000000
-traceFile=wiki2019_remapped.tr
+if [ $trace = "youtube" ]; then 
+	cacheSize=$((1024 * 1024 * 1024 * 142))
+	n_early_stop=$(( 2 * 1000 * 1000 * 1000 ))
+	traceFile="2020-01-19-20-21-22_samesize.tr"
+	results_dir="/home/theano/results/webcachesim_results/results_yt2020"
+elif [ $trace = "wiki" ]; then 
+	cacheSize=$((1024 * 1024 * 1024 * 78))
+	n_early_stop=$(( 1000 * 1000 * 1000 ))
+	traceFile="wiki2019_remapped.tr"
+	results_dir="/home/theano/results/webcachesim_results/results_wiki"
+else
+	cacheSize=$((1024 * 1024 * 1024 * 142))
+	n_early_stop=$(( 500 * 1000 * 1000 ))
+	traceFile="ntg1_500m_base.tr.reindex.annot"
+	results_dir="/home/theano/results/webcachesim_results/results_ntg1"
+fi
 
-cacheType=GDSF
-webcachesim_cli $traceFile $cacheType $cacheSize --n_early_stop=$nearly_stop --enable_trace_format_check=0 &> wcs_gdsf_log_wiki
+for cacheSize in $((1024 * 1024 * 1024 * 46)) $((1024 * 1024 * 1024 * 78)) $((1024 * 1024 * 1024 * 142)); do 
+	echo "Cache size $cacheSize..."
 
-cacheType=Belady
-webcachesim_cli $traceFile $cacheType $cacheSize --n_early_stop=$nearly_stop --enable_trace_format_check=0 &> wcs_belady_log_wiki
+	for cacheType in "GDSF" "Belady" "S2LRU"; do 
+		echo "\tCache type $cacheType..."
+		webcachesim_cli $traceFile $cacheType $cacheSize --n_early_stop=$n_early_stop --enable_trace_format_check=0 > "$results_dir"/"$cacheType"_"$cacheSize".log
 
-cacheType=S2LRU
-webcachesim_cli $traceFile $cacheType $cacheSize --n_early_stop=$nearly_stop --enable_trace_format_check=0 &> wcs_s2lru_log_wiki
-
+	done
+done
