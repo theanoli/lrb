892fd7abd653626ba609c0166d704b3d165acf41
Theano <theanosaurus@gmail.com>
2022-01-20 12:14:54 -0500

    fix merge conflicts

--- runscript.sh
+++ runscript.sh
@@ -4,38 +4,36 @@ export PATH="$root/build/bin"
 export WEBCACHESIM_TRACE_DIR="/home/theano/traces"
 export WEBCACHESIM_ROOT=$root
 
-trace="ntg1"
+trace="$1"
 
+n_early_stop=-1
 if [ $trace = "youtube" ]; then 
 	cacheSize=$((1024 * 1024 * 1024 * 142))
-	n_early_stop=$(( 3 * 1000 * 1000 * 1000 ))
-	traceFile="2020-01-19-20-21-22_samesize.tr.annot"
-	results_dir="/home/theano/results/webcachesim_results/results_yt2020"
+	traceFile="2020-01-19-20-21-22_samesize.tr"
+	results_dir="/home/theano/results/webcachesim_results/youtube_results"
 elif [ $trace = "youtube_capped" ]; then 
 	cacheSize=$((1024 * 1024 * 1024 * 78))
-	n_early_stop=$(( 500 * 1000 * 1000 ))
 	traceFile="2020-01-19-20-21-22_samesize.tr.cappedsz"
-	results_dir="/home/theano/results/webcachesim_results/results_yt2020_capped"
+	results_dir="/home/theano/results/webcachesim_results/youtube_capped_results"
 elif [ $trace = "wiki" ]; then 
 	cacheSize=$((1024 * 1024 * 1024 * 78))
-	n_early_stop=$(( 3000 * 1000 * 1000 ))
 	traceFile="wiki2019_remapped.tr"
 	results_dir="/home/theano/results/webcachesim_results/wiki_results"
 elif [ $trace = "wiki_capped" ]; then 
 	cacheSize=$((1024 * 1024 * 1024 * 78))
-	n_early_stop=$(( 1000 * 1000 * 1000 ))
 	traceFile="wiki2019_remapped.tr.cappedsz"
 	results_dir="/home/theano/results/webcachesim_results/wiki_capped_results"
 elif [ $trace = "ntg1_capped" ]; then 
 	cacheSize=$((1024 * 1024 * 1024 * 78))
-	n_early_stop=$(( 1000 * 1000 * 1000 ))
 	traceFile="ntg1_500m_base.tr.reindex.annot.cappedsz"
-	results_dir="/home/theano/results/webcachesim_results/results_ntg1_capped"
-else
+	results_dir="/home/theano/results/webcachesim_results/ntg1_capped_results"
+elif [ $trace = "ntg1" ]; then 
 	cacheSize=$((1024 * 1024 * 1024 * 142))
-	n_early_stop=$(( 5000 * 1000 * 1000 ))
 	traceFile="ntg1_500m_base.tr.reindex.annot.threecol"
 	results_dir="/home/theano/results/webcachesim_results/ntg1_results"
+else 
+	echo "Unrecognized tracename! Exiting..."
+	exit
 fi
 
 for size in 256; do 
@@ -57,6 +55,5 @@ for size in 256; do
 	#for cacheType in "GDSF" "Belady" "S2LRU" "Inf"; do 
 	#	echo "\tCache type $cacheType..."
 	#	webcachesim_cli $traceFile $cacheType $cacheSize --n_early_stop=$n_early_stop --enable_trace_format_check=0 > "$results_dir"/"$cacheType"_"$cacheSize".log
-
 	#done
 done
